96c5ee4a7593806cf047e775da5d596cb17d7631
Vitalijs Karpusa <v.karpusha@gmail.com>
2016-06-08 22:50:45 +0300

    feat: Add active item in menu, update style

--- imports/ui/components/TopMenu.jsx
+++ imports/ui/components/TopMenu.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+let isActive = (name) => {
+    return (FlowRouter.getRouteName()===name ? 'top-menu__link is-active' : 'top-menu__link');
+};
+
 export const TopMenu = () => (
     <div className="top-menu">
-        <a className="top-menu__link" href={ FlowRouter.path('index') }>Mainpage</a>
-        <a className="top-menu__link" href={ FlowRouter.path('about') }>About</a>
-        <a className="top-menu__link" href={ FlowRouter.path('contact') }>Contact</a>
+        <a className={ isActive('index') } href={ FlowRouter.path('index') }>Mainpage</a>
+        <a className={ isActive('about') } href={ FlowRouter.path('about') }>About</a>
+        <a className={ isActive('contact') } href={ FlowRouter.path('contact') }>Contact</a>
     </div>
 );
--- imports/ui/layouts/Footer.jsx
+++ imports/ui/layouts/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Footer = () => (
     <div className="footer">
         <div className="container">
-            <p>Created by <a href="http://www.karpusa.lv" title="Link" target="_blank">karpusa.lv</a></p>
+            <p>Created by <a href="http://www.karpusa.lv" title="Link" target="_blank">karpusa.lv</a>, email <a href="mailto:vkarpusha@gmail.com">vkarpusha@gmail.com</a></p>
         </div>
     </div>
 );
